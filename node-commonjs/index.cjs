fa0406879650092d267ebfd256f238b556f76236
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-04-28 14:59:55 +0200

    Dynamic import for ES module in CJS

--- node-commonjs/index.cjs
+++ node-commonjs/index.cjs
@@ -1,12 +1,15 @@
-const Module = require('verovio/wasm/verovio-toolkit-wasm-hum.js');
+// const Module = require('verovio/wasm/verovio-toolkit-wasm-hum.js');
 const { VerovioToolkit } = require('verovio');
 const fs = require('fs');
 
-Module().then((VerovioModule) => {
-    const verovioToolkit = new VerovioToolkit(VerovioModule);
-    console.log(verovioToolkit.getVersion());
-    const score = fs.readFileSync('demo.krn');
-    verovioToolkit.loadData(score);
-    const data = verovioToolkit.renderToSVG(1, {});
-    console.log(data);
+import('verovio/wasm/verovio-toolkit-wasm-hum.js').then(Module => {
+    console.log(Module.default);
+    Module.default().then((VerovioModule) => {
+        const verovioToolkit = new VerovioToolkit(VerovioModule);
+        console.log(verovioToolkit.getVersion());
+        const score = fs.readFileSync('demo.krn');
+        verovioToolkit.loadData(score);
+        const data = verovioToolkit.renderToSVG(1, {});
+        console.log(data);
+    });
 });
