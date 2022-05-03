9675256634e06b913b48f0841e009d5b1b809407
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-05-03 13:40:12 +0200

    Fix missing toString

--- node-commonjs/index.cjs
+++ node-commonjs/index.cjs
@@ -7,7 +7,7 @@ import('verovio/wasm/verovio-toolkit-wasm-hum.js').then(Module => {
     Module.default().then((VerovioModule) => {
         const verovioToolkit = new VerovioToolkit(VerovioModule);
         console.log(verovioToolkit.getVersion());
-        const score = fs.readFileSync('demo.krn');
+        const score = fs.readFileSync('demo.krn').toString();
         verovioToolkit.loadData(score);
         const data = verovioToolkit.renderToSVG(1, {});
         console.log(data);
