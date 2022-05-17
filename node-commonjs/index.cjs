d9542f7fd00536ea2ce991f2557fca6f8e8c2d16
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-05-18 01:10:22 +0200

    Improve node-commonjs example

--- node-commonjs/index.cjs
+++ node-commonjs/index.cjs
@@ -1,15 +1,12 @@
-// const Module = require('verovio/wasm/verovio-toolkit-wasm-hum.js');
+const Module = require('verovio/wasm');
 const { VerovioToolkit } = require('verovio');
 const fs = require('fs');
 
-import('verovio/wasm/verovio-toolkit-wasm-hum.js').then(Module => {
-    console.log(Module.default);
-    Module.default().then((VerovioModule) => {
-        const verovioToolkit = new VerovioToolkit(VerovioModule);
-        console.log(verovioToolkit.getVersion());
-        const score = fs.readFileSync('demo.krn').toString();
-        verovioToolkit.loadData(score);
-        const data = verovioToolkit.renderToSVG(1, {});
-        console.log(data);
-    });
-});
+Module.onRuntimeInitialized = () => {
+    const verovioToolkit = new VerovioToolkit(Module);
+    console.log(verovioToolkit.getVersion());
+    const score = fs.readFileSync('demo.krn').toString();
+    verovioToolkit.loadData(score);
+    const data = verovioToolkit.renderToSVG(1, {});
+    console.log(data);
+};
