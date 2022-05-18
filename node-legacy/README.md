070b3ef643e96b96044482ff1a1b867876613c9b
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-05-18 22:42:12 +0200

    Add node legacy example

---
+++ node-legacy/README.md
@@ -0,0 +1,9 @@
+# node-legacy
+
+```sh
+npm install
+node ./verovio-toolkit-hum.js
+node ./verovio-toolkit-light.js
+node ./verovio-toolkit-wasm.js
+node ./verovio-toolkit.js
+```
---
+++ node-legacy/verovio-toolkit.js
@@ -0,0 +1,11 @@
+const verovio = require('verovio/legacy');
+const fs = require('fs');
+
+// verovio.module.onRuntimeInitialized = () => {
+    const verovioToolkit = new verovio.toolkit();
+    console.log(verovioToolkit.getVersion());
+    const score = fs.readFileSync('demo.mei').toString();
+    verovioToolkit.loadData(score);
+    const data = verovioToolkit.renderToSVG(1, {});
+    console.log(data);
+// };
