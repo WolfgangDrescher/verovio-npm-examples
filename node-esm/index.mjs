358dcbdb7f0f4b1e42d38c3b4b9d7eb9792d47cf
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-05-23 13:10:51 +0200

    Change score to mei

--- node-esm/index.mjs
+++ node-esm/index.mjs
@@ -5,7 +5,7 @@ import fs from 'node:fs';
 Module.onRuntimeInitialized = () => {
     const verovioToolkit = new VerovioToolkit(Module);
     console.log(verovioToolkit.getVersion());
-    const score = fs.readFileSync('demo.krn').toString();
+    const score = fs.readFileSync('demo.mei').toString();
     verovioToolkit.loadData(score);
     const data = verovioToolkit.renderToSVG(1, {});
     console.log(data);
