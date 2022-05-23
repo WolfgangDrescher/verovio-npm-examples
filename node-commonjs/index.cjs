861dabfed2d7fa8a7a39bd79e744feda26ab7900
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2022-05-23 12:10:02 +0200

    Fix demo

--- node-commonjs/index.cjs
+++ node-commonjs/index.cjs
@@ -5,7 +5,7 @@ const fs = require('fs');
 Module.onRuntimeInitialized = () => {
     const verovioToolkit = new VerovioToolkit(Module);
     console.log(verovioToolkit.getVersion());
-    const score = fs.readFileSync('demo.krn').toString();
+    const score = fs.readFileSync('demo.mei').toString();
     verovioToolkit.loadData(score);
     const data = verovioToolkit.renderToSVG(1, {});
     console.log(data);
